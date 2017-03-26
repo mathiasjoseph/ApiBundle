4b31b40bd4b8733ed41704406794e4887f7a7fad
miky <mathias89100@gmail.com>
2017-03-26 10:19:25 +0200

    initial commit

---
+++ AdevisApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Miky\Bundle\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MikyApiBundle extends Bundle
+{
+}
---
+++ Controller/ApiController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miky
+ * Date: 15/11/16
+ * Time: 21:55
+ */
+
+namespace Miky\Bundle\ApiBundle\Controller;
+
+
+use FOS\RestBundle\Controller\FOSRestController;
+
+class ApiController extends FOSRestController
+{
+
+}
